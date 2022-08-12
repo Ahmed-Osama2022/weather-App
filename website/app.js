811e21c68ebfd26abea939b09595fa383791a192
Ahmed-Osama2022 <106707667+Ahmed-Osama2022@users.noreply.github.com>
2022-08-12 05:41:41 +0200

    Add files via upload

---
+++ website/app.js
@@ -0,0 +1,86 @@
+/* Global Variables */
+// const baseURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901?';
+let baseURL = 'http://api.openweathermap.org/data/2.5/forecast?zip=';
+let apiKey = '&appid=562674457583c1acb4d4f565094d8ab3';
+
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+/** 
+ * Create an event listener for the element with the id: "generate",
+ *  with a callback function to execute when it is clicked.  
+ */
+ 
+// Let's select the element first ...
+const myButton = document.getElementById('generate');
+// Then add an event listener
+myButton.addEventListener('click', doWork);
+
+/* function called by my Event listener */
+function doWork(e) {
+    const myZip = document.getElementById('zip').value;
+    const myFeeling = document.getElementById('feelings').value;
+    getMyWeather(baseURL, myZip, apiKey)
+
+        .then(function(data) {
+            console.log(data);
+            // Add The Data to POST request:
+            postMyData('/addNewData', {date:d, temp: data.list[0].main.temp, content: myFeeling })
+            updateEl();
+        })
+};
+        
+
+
+
+/* Function to GET web API Data */
+const getMyWeather = async (baseURL, zipCode, key) => {
+
+    const response = await fetch(baseURL + zipCode + key);
+    try {
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log("There is an Error", error);
+    }
+}  
+
+const postMyData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST',  
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header 
+    // We can convert a JavaScript object into a string using the JavaScript method       // 
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      } catch(error) {
+      console.log("error", error);
+      }
+  }
+
+/* function to GET Project Data */
+
+const updateEl = async () => {
+    const request = await fetch('/all');
+    try {
+        const allMyData = await request.json();
+        document.getElementById('date').innerHTML = `Date: ${allMyData[0].date}`;
+        document.getElementById('temp').innerHTML = `Temperature: ${allMyData[0].temp}`;
+        document.getElementById('content').innerHTML = `I feel like: ${allMyData[0].content}`;
+       
+    } catch(error) {
+        console.log("There is an Error", error);
+    }
+};
